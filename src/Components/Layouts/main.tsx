652e78c87224a1f721401b23cafd66a4bec4692c
Kryštof Krátký <krystof.kratky2003@gmail.com>
2022-02-10 10:29:55 +0100

    bootstrap sidebar

--- src/Components/Layouts/main.tsx
+++ src/Components/Layouts/main.tsx
@@ -1,15 +1,19 @@
-import React from "react"
+import React from "react";
+import Sidebar from "../Modules/Sidebar";
 
 type MainLayoutProps = {
-    children: React.ReactNode
-}
+  children: React.ReactNode;
+};
 
-const MainLayout:React.FC<MainLayoutProps> = ({children}) => {
-    return (
-        <div>
-            {children}
-        </div>
-    )
-}
+const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
+  return (
+    <div className="vh-100" >
+      <div className="row h-100">
+        <Sidebar />
+        <section className="col"> {children}</section>
+      </div>
+    </div>
+  );
+};
 
-export default MainLayout
+export default MainLayout;
