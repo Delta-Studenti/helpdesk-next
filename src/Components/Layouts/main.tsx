fcc775c651a580e902f989e6226753b0331cc90e
Kryštof Krátký <krystof.kratky2003@gmail.com>
2022-02-10 11:43:28 +0100

    navbar

--- src/Components/Layouts/main.tsx
+++ src/Components/Layouts/main.tsx
@@ -1,24 +1,30 @@
-import Head  from "next/head";
+import Head from "next/head";
 import React from "react";
 import { SidebarItemType } from "../../types/sidebar";
 import { Sidebar } from "../Modules/Sidebar";
+import { Topbar } from "../Modules/Topbar";
 
 type MainLayoutProps = {
   children: React.ReactNode;
   title: string;
-  sidebarTab?: SidebarItemType
+  sidebarTab?: SidebarItemType;
 };
 
-const MainLayout: React.FC<MainLayoutProps> = ({ children, title, sidebarTab }) => {
+const MainLayout: React.FC<MainLayoutProps> = ({
+  children,
+  title,
+  sidebarTab,
+}) => {
   return (
     <>
       <Head>
         <title>{title}</title>
         <meta property="og:title" content={title} key="title" />
       </Head>
+      <Topbar />
       <div className="container p-0 m-0">
         <div className="row">
-          <Sidebar sidebarTab={sidebarTab}/>
+          <Sidebar sidebarTab={sidebarTab} />
           <main className="col overflow-auto">{children} </main>
         </div>
       </div>
--- src/Components/Modules/Sidebar/Sidebar.tsx
+++ src/Components/Modules/Sidebar/Sidebar.tsx
@@ -6,30 +6,26 @@ import { AiOutlineUnorderedList, AiOutlinePlusCircle } from "react-icons/ai";
 import { SidebarItemType } from "../../../types/sidebar";
 
 type SidebarProps = {
-  sidebarTab?: SidebarItemType
+  sidebarTab?: SidebarItemType;
+};
 
-}
-
-const Sidebar: React.FC<SidebarProps> = ({sidebarTab}) => {
+const Sidebar: React.FC<SidebarProps> = ({ sidebarTab }) => {
   return (
     <div
-      className="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark vh-100 col-sm-3"
+      className="d-flex flex-column flex-shrink-0 p-3 text-dark bg-light vh-100 col-sm-3"
       style={{ width: "280px" }}
     >
-      <Link href="/">
-        <a className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-          <h1> Sidebar</h1>
-        </a>
-      </Link>
-      <hr />
-      <ul className="nav nav-pills flex-column mb-auto gap-1">
+      <ul className="nav nav-pills flex-column mb-auto gap-1 text-dark">
         {/* ALL TICKETS */}
         <SidebarRow link="/" active={sidebarTab === "tickets"}>
           <AiOutlineUnorderedList />
           Tikety
         </SidebarRow>
         {/* CREATE TICKET */}
-        <SidebarRow link="/create-ticket" active={sidebarTab === "create-ticket"}>
+        <SidebarRow
+          link="/create-ticket"
+          active={sidebarTab === "create-ticket"}
+        >
           <AiOutlinePlusCircle />
           Vytvořit tiket
         </SidebarRow>
