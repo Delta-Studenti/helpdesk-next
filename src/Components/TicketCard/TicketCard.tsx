a474a75d4c636cb5e2d5e8075835961a6c59690d
Kryštof Krátký <krystof.kratky2003@gmail.com>
2022-02-10 12:02:07 +0100

    hotfix akorát že vůbec

--- src/Components/TicketCard/TicketCard.tsx
+++ src/Components/TicketCard/TicketCard.tsx
@@ -1,64 +1,72 @@
-import Link from "next/link";
 import React from "react";
-import { ticket } from "../../types/ticket";
+import { Ticket, User } from "../../../.cache/__types__";
 
-const TicketCard: React.FC = (props: ticket) => {
-    const createdAt = new Date(props.ticket.createdAt).toLocaleDateString();
-    return (
-        <div className="d-flex flex-row align-items-center justify-content-between shadow-lg rounded bg-body rounded-3 m-4 ">
-            <div className="col">
-                <input
-                    className="form-check-input"
-                    type="checkbox"
-                    value=""
-                    aria-label="Checkbox for following text input"
-                />
-            </div>
-            <div className="col d-flex justify-content-between">
-                <p className="text-start">{props.ticket.user.firstName}</p>
-                <p>{props.ticket.user.lastName}</p>
-            </div>
-            <div className="col">
-                <p className="text-center">#{props.ticket.id}</p>
-            </div>
-            <div className="col">
-                <p>{props.ticket.title}</p>
-            </div>
-            <div className="col">
-                <span
-                    className={`badge 
+type TicketCardProps = {
+  ticket: { 
+    id: number
+    status: string 
+    title: string
+    user: {firstName:string, lastName: string};
+  }
+};
+
+const TicketCard: React.FC<TicketCardProps> = ({ ticket }) => {
+  const createdAt = new Date(/*ticket.createdAt*/).toLocaleDateString();
+  return (
+    <div className="d-flex flex-row align-items-center justify-content-between shadow-lg rounded bg-body rounded-3 m-4 ">
+      <div className="col">
+        <input
+          className="form-check-input"
+          type="checkbox"
+          value=""
+          aria-label="Checkbox for following text input"
+        />
+      </div>
+      <div className="col d-flex justify-content-between">
+        <p className="text-start">{ticket.user.firstName}</p>
+        <p>{ticket.user.lastName}</p>
+      </div>
+      <div className="col">
+        <p className="text-center">#{ticket.id}</p>
+      </div>
+      <div className="col">
+        <p>{ticket.title}</p>
+      </div>
+      <div className="col">
+        <span
+          className={`badge 
                     ${
-                        props.ticket.status == "Otevřený"
-                            ? "bg-warning"
-                            : "bg-secondary"
+                      ticket.status == "Otevřený"
+                        ? "bg-warning"
+                        : "bg-secondary"
                     }
                     ${
-                        props.ticket.status == "Vyřešený"
-                            ? "bg-success"
-                            : "bg-secondary"
+                      ticket.status == "Vyřešený"
+                        ? "bg-success"
+                        : "bg-secondary"
                     }
                     `}
-                >
-                    {props.ticket.status}
-                </span>
-            </div>
-            <div className="col">
-                <p>{createdAt}</p>
-            </div>
-            <div className="col">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    className="bi bi-three-dots"
-                    viewBox="0 0 16 16"
-                >
-                    <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z" />
-                </svg>
-            </div>
-        </div>
-    );
+        >
+          {ticket.status}
+        </span>
+      </div>
+      <div className="col">
+        <p>{createdAt}</p>
+      </div>
+      <div className="col">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          className="bi bi-three-dots"
+          viewBox="0 0 16 16"
+        >
+          <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z" />
+        </svg>
+      </div>
+    </div>
+  );
 };
 
 export default TicketCard;
