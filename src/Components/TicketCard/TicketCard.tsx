f768569d177610e0b5e64d026600f118f1cac476
vskorepa <vskorepa@skori.cz>
2022-02-10 11:02:56 +0100

    Ticket_card

--- src/Components/TicketCard/TicketCard.tsx
+++ src/Components/TicketCard/TicketCard.tsx
@@ -1,30 +1,48 @@
 import Link from "next/link";
-import React, {FC} from "react";
+import React from "react";
+import { ticket } from "../../types/ticket";
 
-const TicketCard : FC = () => {
-  return (
-    <>
-      <div className="col">
-        <div className="row pb-3">
-          <div className="card">
-            <div className="card-body">
-              <h5 className="card-title">Nefunkční Firewall</h5>
-              <h6 className="card-subtitle mb-2 text-muted">
-                Autor: Michal Maděra
-              </h6>
-              <p className="card-text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
-                soluta, nisi nemo ipsum error, distinctio aliquid ab quidem
-                omnis aspernatur sit odit expedita earum debitis vero
-                consequuntur similique, facilis ipsam.
-              </p>
-              <span className="badge rounded-pill bg-danger">Urgentní</span>
+const TicketCard: React.FC = (props: ticket) => {
+    return (
+        <div className="d-flex flex-row align-items-start p-4 shadow-lg rounded bg-body rounded-3 m-4">
+            <div className="col">
+                <input
+                    className="form-check-input mt-0"
+                    type="checkbox"
+                    value=""
+                    aria-label="Checkbox for following text input"
+                />
             </div>
-          </div>
+            <div className="col d-flex flex-row gap-1">
+                <p>{props.ticket.user.firstName}</p>
+                <p>{props.ticket.user.lastName}</p>
+            </div>
+            <div className="col">
+                <p>#{props.ticket.id}</p>
+            </div>
+            <div className="col">
+                <p>#{props.ticket.title}</p>
+            </div>
+            <div className="col">
+                <span
+                    className={`badge 
+                    ${
+                        props.ticket.status == "Otevřený"
+                            ? "bg-warning"
+                            : "bg-secondary"
+                    }
+                    ${
+                        props.ticket.status == "Vyřešený"
+                            ? "bg-success"
+                            : "bg-secondary"
+                    }
+                    `}
+                >
+                    {props.ticket.status}
+                </span>
+            </div>
         </div>
-      </div>
-    </>
-  );
+    );
 };
 
 export default TicketCard;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { GetServerSideProps, NextPage } from "next";
 import {
-  TicketsDocument,
-  useTicketsQuery,
+    TicketsDocument,
+    useTicketsQuery,
 } from "../graphql/frontend/tickets.graphql";
 import { client } from "../lib/apollo-server";
 import Link from "next/link";
@@ -9,40 +9,32 @@ import React from "react";
 import TicketCard from "../Components/TicketCard/TicketCard";
 
 const Home: NextPage = () => {
-  const { data, loading, error } = useTicketsQuery();
-  if (loading) return <p>Loading...</p>;
-  if (error || !data) return <p>Error :(</p>;
-  return (
-    <>
-      <div className="container">
-        <TicketCard/>
-
-        {data.tickets.map((ticket) => (
-        <div key={ticket.id}>
-          <h1>
-            <Link href={`/${ticket.id}`} passHref>
-              <a>{ticket.title}</a>
-            </Link>
-          </h1>
-          <p>{`${ticket.user.firstName} ${ticket.user.lastName}`}</p>
-          <p>{ticket.status}</p>
-        </div>
-      ))}
-      </div>
-    </>
-  );
+    const { data, loading, error } = useTicketsQuery();
+    if (loading) return <p>Loading...</p>;
+    if (error || !data) return <p>Error :(</p>;
+    return (
+        <>
+            <table className="table">
+                <tbody>
+                    {data.tickets.map((ticket) => (
+                        <TicketCard key={ticket.id} ticket={ticket} />
+                    ))}
+                </tbody>
+            </table>
+        </>
+    );
 };
 
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
-  await client.query({
-    query: TicketsDocument,
-  });
+    await client.query({
+        query: TicketsDocument,
+    });
 
-  return {
-    props: {
-      initialApolloState: client.cache.extract(),
-    },
-  };
+    return {
+        props: {
+            initialApolloState: client.cache.extract(),
+        },
+    };
 };
 
 export default Home;
